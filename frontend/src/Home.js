85c234ee77f1700b9ace044c45e6325c0a229fa4
vikashzapbuild <vikash.zapbuild@gmail.com>
2020-05-20 18:14:26 +0530

    login signup button and search box added

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
+import ReactSearchBox from 'react-search-box'
+
 import './Home.css';
-import food from './images/food.jpeg'
 
 export default class Home extends Component {
+    data = [
+        {
+          key: 'john',
+          value: 'John Doe',
+        },
+        {
+          key: 'jane',
+          value: 'Jane Doe',
+        },
+        {
+          key: 'mary',
+          value: 'Mary Phillips',
+        },
+        {
+          key: 'robert',
+          value: 'Robert',
+        },
+        {
+          key: 'karius',
+          value: 'Karius',
+        },
+      ]
     render() {
         return (
             <div className="Home">
-             <img src={food} className="App-logo" alt="logo" />
+             <button className="login" href="#">Login</button>
+             <button className="signup" href="#">Signup</button>
+             <div className="header">Food Delivery</div>
+             <ReactSearchBox
+                placeholder="Search Your food here"
+                data={this.data}
+                callback={record => console.log(record)}
+                className="search"
+            />
+            <button>Search</button>
             </div>
         )
     }
