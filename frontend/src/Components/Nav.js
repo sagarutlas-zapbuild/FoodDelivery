9877154c8d6bae2f3a151f528bab8a5cd39ae7f6
vikashzapbuild <vikash.zapbuild@gmail.com>
2020-05-27 22:43:41 +0530

    formation changes

--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {NavItem, NavLink, NavbarToggler, Nav } from 'reactstrap';
+import { NavItem, NavLink, NavbarToggler, Nav } from 'reactstrap';
 
 const TopNav = () => {
     if (localStorage.getItem('token') ? true : false) {
@@ -7,23 +7,23 @@ const TopNav = () => {
 
             <NavbarToggler aria-controls="responsive-navbar-nav" />
 
-                <Nav className="mr-auto">
-                    <NavItem>
-                        <NavLink href="/Home" style={{marginLeft: "750px",marginTop:"20px"}}
+            <Nav className="mr-auto">
+                <NavItem>
+                    <NavLink href="/Home" style={{ marginLeft: "750px", marginTop: "20px" }}
                         className="btn btn-primary">Home</NavLink>
-                    </NavItem>
+                </NavItem>
 
-                    <NavItem>
-                        <NavLink href="/me" style={{marginLeft: "40px",marginTop:"20px"}}
+                <NavItem>
+                    <NavLink href="/me" style={{ marginLeft: "40px", marginTop: "20px" }}
                         className="btn btn-primary">Hello {localStorage.getItem('user_name')}</NavLink>
-                    </NavItem>
+                </NavItem>
 
-                    <NavItem>
-                        <NavLink href="/cart" style={{marginLeft: "40px",marginTop:"20px"}}
+                <NavItem>
+                    <NavLink href="/cart" style={{ marginLeft: "40px", marginTop: "20px" }}
                         className="btn btn-primary">Cart</NavLink>
-                    </NavItem>
+                </NavItem>
 
-                </Nav>
+            </Nav>
 
         </>);
     }
@@ -34,11 +34,11 @@ const TopNav = () => {
             <Nav className="mr-auto">
                 <NavItem>
                     <NavLink href="/Signup" className="btn btn-primary"
-                    style={{marginLeft: "750px",marginTop:"20px"}}>Signup</NavLink>
+                        style={{ marginLeft: "750px", marginTop: "20px" }}>Signup</NavLink>
                 </NavItem>
                 <NavItem>
                     <NavLink href="/Login" className="btn btn-primary"
-                    style={{marginLeft: "40px",marginTop:"20px"}}>Login</NavLink>
+                        style={{ marginLeft: "40px", marginTop: "20px" }}>Login</NavLink>
                 </NavItem>
             </Nav>
 
