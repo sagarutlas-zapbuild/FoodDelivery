8448956fad91584820e57f0792dff292c0ac00cd
vikashzapbuild <vikash.zapbuild@gmail.com>
2020-05-30 13:08:18 +0530

    spelling correction in urls.js

--- frontend/src/Urls.js
+++ frontend/src/Urls.js
@@ -1,5 +1,5 @@
 export const loginUrl = "http://localhost:8000/accounts/auth/";
 export const signupUrl = "http://localhost:8000/accounts/users/";
-export const restaurantUrl = "http://localhost:8000/reastaurants/";
+export const restaurantUrl = "http://localhost:8000/restaurants/";
 export const cartUrl = "http://127.0.0.1:8000/carts/";
 export const orderUrl =  "http://localhost:8000/item_ratings/";
